0f206283e280a4d11e9284d45be3b962b324cbb6
Tiochan <tiochan@gmail.com>
2019-04-25 10:50:35 +0200

    jue abr 25 10:50:35 CEST 2019

---
+++ include/cron/tag_launcher.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Jorge Novoa (jorge.novoa@upcnet.es)
+ * For: Politechnical University of Catalonia (UPC), Spain.
+ *
+ * @package discoverer
+ * @subpackage cron
+ *
+ * Script to detect launchs
+ */
+
+
+	define("CLI_REQUIRED",true);
+
+	$dir= dirname($_SERVER["PHP_SELF"]);
+	define("SYSHOME", $dir . "/../..");
+
+
+	include_once SYSHOME . "/include/init.inc.php";
+	include_once INC_DIR . "/reports/reports.class.php";
+
+
+	//$report= new reports_to_launch();
+	$subscriptions= new subscriptions_to_launch();
+	$subscriptions->launch();
